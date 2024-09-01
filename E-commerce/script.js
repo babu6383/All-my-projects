3051dadf14bdae803fbf6f0f0d5edfeb01ded586
babu6383 <dineshbabu1748@gmail.com>
2024-09-01 10:49:00 +0530

    added new projects

---
+++ E-commerce/script.js
@@ -0,0 +1,63 @@
+
+
+//leap year
+
+// let year= 2024
+
+// if(year%4==0  || year%400==0 && year %100!=0){
+//     console.log("it is leep year");
+
+// }
+// else {
+//    console.log(" it is not leep year");
+// }
+
+
+
+//positive negative
+
+// let num=8
+
+// if(num<0) {
+//     console.log("number is negative");
+
+// }
+// else if(num>0){
+//     console.log("number is positive")
+// }
+// else{
+//    console.log("number is nutral") 
+// }
+
+
+//grade ststem
+// let num=30
+
+// if(num>=1 && num<=100){
+
+//     if(num>=50 && num<=60 ){
+//         console.log("third class")
+//     }
+
+//     else if(num>=60 && num<=70){
+//         console.log("secound class")
+//     }
+//     else if (num>=70 && num<=100){
+//         console.log("first class")
+//     }
+//    else{
+//     console.log("fail")
+//    }
+// }
+
+// else{
+//     console.log("invalid")
+// }
+
+
+let stdname="sai"
+
+let stddepart="cse"
+console.log(`the name is ${stdname} the department is ${stddepart}`)
+console.log("the name is " +stdname +" the department is "+stddepart)
+
